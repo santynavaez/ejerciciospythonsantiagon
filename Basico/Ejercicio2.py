a6cde20a3760c24d266e9fbe1cf3f97a66080411
santynarvaez <santynarvaez9@gmail.com>
2026-06-18 12:12:24 -0500

    segundo commit

--- Basico/Ejercicio2.py
+++ Basico/Ejercicio2.py
@@ -6,3 +6,6 @@ print("el valor de la variable nombre es:" , nombre)
 
 numero=int(input("digite el numero: "))
 print("el valor de la variable es: " , numero)
+
+flotante=float(input("Digite el numero flotante"))
+print("el valor de la variable flotante es" , flotante)
